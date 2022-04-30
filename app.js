e2b3491216369a6ae63b73fd57db1e4f3dc3aedf
Sachin Kanyal <2000sachinkanyal@gmail.com>
2022-04-30 19:07:44 +0530

    adding hashing plus salting

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express=require('express');
 const bp=require('body-parser');
 const app=express();
 const mongoose=require('mongoose');
 const encrypt=require('mongoose-encryption');
 const md5=require('md5');
+const bcrypt=require('bcrypt');
+const salt=10;
 mongoose.connect('mongodb://localhost/credit');
 const schs=new mongoose.Schema({
     email:String,
     password:String
 });
-//console.log(process.env);
-//schs.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:['password']});
 const mdl=mongoose.model('register',schs);
 app.use(express.static('public'));
 app.set('view engine','ejs');
@@ -26,29 +26,41 @@ app.get('/register',function(req,res){
     res.render('register');
 });
 app.post('/register',function(req,res){
-  //  console.log(req.body);
     const em=req.body.username;
-    const pa=md5(req.body.password);
-    const dt=new mdl({
-        email:em,
-        password:pa
+    const pa=req.body.password;
+    bcrypt.hash(pa,salt,function(err,result){
+        if(err)
+        console.log(err);
+        else{
+            const dt=new mdl({
+                email:em,
+                password:result
+            });
+            dt.save();
+            res.redirect('/');
+        }
     });
-    dt.save();
-    res.redirect('/');
 });
 app.post('/login',function(req,res){
     const a=req.body.username;
-    const b=md5(req.body.password);
+    const b=req.body.password;
     mdl.findOne({email:a},function(err,result){
         if(err)
         console.log(err);
         else{
             if(result)
             {
-                if(result.password===b)
-                res.render('secrets');
-                else
-                res.redirect('/login');
+                
+                    bcrypt.compare(b,result.password,function(err,result){
+                        if(err)
+                        console.log(err);
+                        else if(result==true)
+                        res.render('secrets');
+                        else
+                        res.redirect('/login');
+                    });
+             
+                
             }
             else
             res.redirect('/login');
